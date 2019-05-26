0e91262bded6c30c2b9d1d34fdf3273ee71dbd88
Click Here <dhdong1510@gmail.com>
2019-05-26 23:51:21 +0700

    init project

---
+++ laraecom/resources/lang/en/auth.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'authenticate'     => 'Authenticate',
+    'create'           => 'Create an account',
+    'email'            => 'Email',
+    'password'         => 'Password',
+    'change_password'  => 'Change password',
+    'current_password' => 'Current password',
+    'new_password'     => 'New password',
+    'confirm_password' => 'Confirm password',
+    'failed'           => 'These credentials do not match our records.',
+    'reset_password'   => 'Reset your password',
+    'login'            => 'Login',
+    'remember'         => 'Rememer me',
+    'throttle'         => 'Too many login attempts. Please try again in :seconds seconds.',
+    'social_auth'      => 'or use your account from',
+    'save'             => 'Save settings',
+    'signin'           => 'Sign in',
+    'signin_title'     => 'Sign in with your account',
+    'facebook'         => 'Login with Facebook',
+    'google'           => 'Login with Google',
+    'twitter'          => 'Login with Twitter',
+];
