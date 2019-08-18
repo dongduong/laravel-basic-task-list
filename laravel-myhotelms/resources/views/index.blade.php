c5d200235ae019f6358845c754813f5822b37eb9
Dong Duong <dong.jobready@gmail.com>
2019-08-18 15:55:25 +0700

    parital hotel menu blade

--- laravel-myhotelms/resources/views/index.blade.php
+++ laravel-myhotelms/resources/views/index.blade.php
@@ -1,26 +1,7 @@
 @extends('layouts.home')
 
 @section('main')
-  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div class="container">
-      <a class="navbar-brand" href="/">BrightHotel</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="oi oi-menu"></span> Menu
-      </button>
-
-      <div class="collapse navbar-collapse" id="ftco-nav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item active"><a href="/" class="nav-link">Home</a></li>
-          <li class="nav-item"><a href="{{ route('rooms.index') }}" class="nav-link">Rooms</a></li>
-          <li class="nav-item"><a href="{{ route('services.index') }}" class="nav-link">Services</a></li>
-          <li class="nav-item"><a href="{{ route('about.index') }}" class="nav-link">About Us</a></li>
-          <li class="nav-item"><a href="{{ route('blog.index') }}" class="nav-link">Blog</a></li>
-          <li class="nav-item"><a href="{{ route('contact.index') }}" class="nav-link">Contact</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- END nav -->
+  @include('partials.hotel-menu')
   
   <div class="block-31" style="position: relative;">
     <div class="owl-carousel loop-block-31 ">
---
+++ laravel-myhotelms/resources/views/partials/hotel-menu.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="/">Bright Hotel</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="oi oi-menu"></span> Menu
+      </button>
+
+      <div class="collapse navbar-collapse" id="ftco-nav">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item {{ request()->is('/') ? 'active' : '' }}"><a href="/" class="nav-link">Home</a></li>
+          <li class="nav-item {{ request()->is('rooms') || request()->is('rooms/*') ? 'active' : '' }}"><a href="{{ route('rooms.index') }}" class="nav-link">Rooms</a></li>
+          <li class="nav-item {{ request()->is('services') || request()->is('services/*') ? 'active' : '' }}"><a href="{{ route('services.index') }}" class="nav-link">Services</a></li>
+          <li class="nav-item"><a href="{{ route('about.index') }}" class="nav-link">About Us</a></li>
+          <li class="nav-item"><a href="{{ route('blog.index') }}" class="nav-link">Blog</a></li>
+          <li class="nav-item"><a href="{{ route('contact.index') }}" class="nav-link">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+</nav>
+  <!-- END nav -->
